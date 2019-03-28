ae51e575a36cd9e1f64bad1828c29b0a4aeb5833
Hannah Gramm <hannah.gramm@tum.de>
2019-03-28 19:11:52 +0100

    upload1

---
+++ js/script.js
@@ -0,0 +1,37 @@
+
+console.log("App is alive");
+
+// writes the name of the clicked channel on the app bar
+function switchChannel(channelName) {
+    //Log to  switch
+    console.log("Tuning in to channel", channelName);
+    //new channel to right appbar
+    document.getElementById('channel-name').innerHTML = channelName;
+    document.getElementById('channel-location').innerHTML = 'by <a href="http://w3w.co/cheeses.yard.applies" target="_blank"><strong>cheeses.yard.applies</strong></a>';
+    //star when clicking
+    $('#channel-star').attr('src', 'http://ip.lfe.mw.tum.de/sections/star-o.png');
+    // active state for channel
+    $('#channels li').removeClass('selected');
+    $('#channels li:contains(' + channelName + ')').addClass('selected');
+}
+
+//star when clicking
+function star() {
+    $('#channel-star').attr('src', 'http://ip.lfe.mw.tum.de/sections/star.png');
+}
+
+// switches between the tab buttons: add and remove .selected
+function selectTab(tabId) {
+
+    $('#tab-bar button').removeClass('selected');
+
+    console.log('Changing to tab', tabId);
+
+    $(tabId).addClass('selected');
+}
+
+//switching/toggling emojis
+function toggleEmojis() {
+    /* $('#emojis').show(); // #show */
+    $('#emojis').toggle(); // #toggle
+}
